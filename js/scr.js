68998428a0b0b04519044633955d7d7de4ec7912
Ivan Buncic <ivan@triplsi.com>
2018-06-07 13:58:14 +0200

    Update scr.js

--- js/scr.js
+++ js/scr.js
@@ -1,7 +1,1 @@
-var navMenu = document.getElementsByClassName('navigation__background');
-var menuOpened = document.getElementsByClassName('navigation__checkbox:checked');  
-var navLink = document.getElementsByClassName('navigation__link');
-
-navLink.addEventListener('click', function() {
-  navMenu.classList.remove('navigation__checkbox:checked');
-});
+Nothing special :P
