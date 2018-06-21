0ec45bbd7968b097b804513385dad29330a02a95
Ivan Buncic <ivan@triplsi.com>
2018-06-22 00:47:31 +0200

    Update scr.js

--- js/scr.js
+++ js/scr.js
@@ -1,1 +1,1 @@
-console.log("Hello World, support me on https://paypal.me/IBuncic or https://patreon.com/ivanbuncic/memberships. Thank you from the deepest dept of my heart. Deepest bow. And a kiss.")
+console.log("Hello World, support me here --> https://paypal.me/IBuncic or https://patreon.com/ivanbuncic/memberships. Thank you from the deepest depth of my heart. Deepest bow. And a kiss.")
