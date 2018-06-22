c6592670538c4539858b67b98f3c9b9b5a37c527
Ivan Buncic <ivan@triplsi.com>
2018-06-22 02:52:34 +0200

    Update scr.js
    Lazy to do more. Sofija was low. Going to sleep

--- js/scr.js
+++ js/scr.js
@@ -1,1 +1,4 @@
+$( "li" ).on("click", function() {
+console.log("Li clicked!");
+}); // Lazy to do more. Sofija was low. Going to sleep
 console.log("Hello World, support me here --> https://paypal.me/IBuncic or https://patreon.com/ivanbuncic/memberships. Thank you from the deepest depth of my heart. Deepest bow. And a kiss.")
