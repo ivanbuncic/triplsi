3f118228614f8672db84b87c637a695c1370940b
Ivan Buncic <ivan@triplsi.com>
2018-06-26 02:04:39 +0200

    Update scr.js
    Added class that doesn't work..

--- js/scr.js
+++ js/scr.js
@@ -1,4 +1,5 @@
 $( "li" ).on("click", function() {
-console.log("Li clicked!");
-}); // Lazy to do more. Sofija was low. Going to sleep
+  $(".navigation__nav").toggleClass(".navigation__checkbox:checked");
+});
+
 console.log("Hello World, support me here --> https://paypal.me/IBuncic or https://patreon.com/ivanbuncic/memberships. Thank you from the deepest depth of my heart. Deepest bow. And a kiss.")
