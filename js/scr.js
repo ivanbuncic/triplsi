58b8871d9011484f6a6f63441293066db0aa1238
Ivan Buncic <ivan@triplsi.com>
2018-06-26 02:32:37 +0200

    Update scr.js

--- js/scr.js
+++ js/scr.js
@@ -3,7 +3,7 @@ var menu_button = $(".navigation__checkbox:checked");
 
 list_item.on("click", function() {
   console.log("li clicked");
-  menu_button.toggleClass.(".navigation__checkbox:checked");
+  
 });
 
 console.log("Hello World, support me here --> https://paypal.me/IBuncic or https://patreon.com/ivanbuncic/memberships. Thank you from the deepest depth of my heart. Deepest bow. And a kiss.")
