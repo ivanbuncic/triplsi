f0adc5a92618fe507e3c1e4d3793eb54bfa35ffb
Ivan Buncic <ivan@triplsi.com>
2018-07-02 00:52:07 +0200

    Update scr.js

--- js/scr.js
+++ js/scr.js
@@ -1,5 +1,5 @@
 console.log("Hello World, support me here --> https://paypal.me/IBuncic or https://patreon.com/ivanbuncic/memberships. Thank you from the deepest depth of my heart. Deepest bow. And a kiss.");
  
-$( "nav li" ).click(function() {
+$( ".navigation_link" ).click(function() {
 	$("input[type=checkbox]").click();
 });
